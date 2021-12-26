3e6da3f8e63da2713b6180d2d23b80524dd02be5
SupSkiFun <SupSkiFun@gmail.com>
2021-12-26 13:51:36 -0700

    fix01

--- RunOnWin/SetEnv.ps1
+++
@@ -1,6 +0,0 @@
-$env:DB_NAME="mortgage_info"
-$env:DB_PASS="9544EEiiiiDDDfghIJJklpumpkin"
-$env:DB_TYPE="postgres"
-$env:DB_USER="web_ro"
-$env:DB_HOST="uboo.supskifun.net"
-Start-Job -Name Mortgage { .\winx64_mortgage.exe}
